34e7fec355db0ce6da0e2bc5d84a07f56f4187d6
McDoyen <mrpoloh@yahoo.co.uk>
2017-10-12 15:56:11 +0300

    Make mendix 7 compatibility

--- src/HTMLSnippet/widget/HTMLSnippet.js
+++ src/HTMLSnippet/widget/HTMLSnippet.js
@@ -1,36 +1,44 @@
-dojo.provide("HTMLSnippet.widget.HTMLSnippet");
+define([
+	'dojo/_base/declare',
+	'mxui/widget/_WidgetBase',
+	"dojo/dom-style",
+	"dojo/html",
+	"dojo/dom-attr"
+], function (declare, _WidgetBase, domStyle, html, domAttr) {
+	'use strict';
 
-mendix.widget.declare('HTMLSnippet.widget.HTMLSnippet', {
-	inputargs: { 
-		contenttype : 'html',
-		contents : '',
-		documentation: '',
-		style : ''
-	},
-	
-	postCreate : function(){
-		//houskeeping
-		logger.debug(this.id + ".postCreate");
-		
-		switch(this.contenttype) {
-			case 'html':
-				dojo.style(this.domNode, {'height' : 'auto', 'width' : '100%'});
-				dojo.attr(this.domNode, 'style', this.style); //might override height and widght
-				this.domNode.innerHTML = this.contents;
-				break;
-			case 'js' :
-				try {
-					eval(this.contents);
-				}
-				catch (e) {
-					dojo.html.set(this.domNode, "Error while evaluating JavaScript: " + e );
-				}
-				break;
+	return declare('HTMLSnippet.widget.HTMLSnippet', [ _WidgetBase ], {
+		inputargs: {
+			contenttype: 'html',
+			contents: '',
+			documentation: '',
+			style: ''
+		},
+
+		postCreate: function () {
+			//houskeeping
+			logger.debug(this.id + ".postCreate");
+
+			switch (this.contenttype) {
+				case 'html':
+				domStyle.set(this.domNode, { 'height': 'auto', 'width': '100%' });
+				domAttr.set(this.domNode, 'style', this.style); //might override height and widght
+					this.domNode.innerHTML = this.contents;
+					break;
+				case 'js':
+					try {
+						eval(this.contents);
+					}
+					catch (e) {
+						html.set(this.domNode, "Error while evaluating JavaScript: " + e);
+					}
+					break;
+			}
+		},
+
+		uninitialize: function () {
 		}
-		
-		this.actRendered();
-	},
-	
-	uninitialize : function(){
-	}
-});	
+	});
+});
+
+require([ 'HTMLSnippet/widget/HTMLSnippet' ]);

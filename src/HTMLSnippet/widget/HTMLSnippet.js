ff3afefd9c9c82af8b67bc12acdf3a5ca4355206
McDoyen <mrpoloh@yahoo.co.uk>
2017-10-17 10:12:29 +0300

    Process review

--- src/HTMLSnippet/widget/HTMLSnippet.js
+++ src/HTMLSnippet/widget/HTMLSnippet.js
@@ -5,40 +5,39 @@ define([
 	"dojo/html",
 	"dojo/dom-attr"
 ], function (declare, _WidgetBase, domStyle, html, domAttr) {
-	'use strict';
 
 	return declare('HTMLSnippet.widget.HTMLSnippet', [ _WidgetBase ], {
-		inputargs: {
-			contenttype: 'html',
-			contents: '',
-			documentation: '',
-			style: ''
-		},
-
-		postCreate: function () {
-			//houskeeping
-			logger.debug(this.id + ".postCreate");
-
-			switch (this.contenttype) {
-				case 'html':
-				domStyle.set(this.domNode, { 'height': 'auto', 'width': '100%' });
-				domAttr.set(this.domNode, 'style', this.style); //might override height and widght
-					this.domNode.innerHTML = this.contents;
-					break;
-				case 'js':
-					try {
-						eval(this.contents);
-					}
-					catch (e) {
-						html.set(this.domNode, "Error while evaluating JavaScript: " + e);
-					}
-					break;
-			}
-		},
-
-		uninitialize: function () {
+	inputargs: { 
+		contenttype : 'html',
+		contents : '',
+		documentation: '',
+		style : ''
+	},
+	
+	postCreate : function(){
+		//houskeeping
+		logger.debug(this.id + ".postCreate");
+		
+		switch(this.contenttype) {
+			case 'html':
+			domStyle.set(this.domNode, {'height' : 'auto', 'width' : '100%'});
+			domAttr.set(this.domNode, 'style', this.style); //might override height and widght
+				this.domNode.innerHTML = this.contents;
+				break;
+			case 'js' :
+				try {
+					eval(this.contents);
+				}
+				catch (e) {
+					html.set(this.domNode, "Error while evaluating JavaScript: " + e );
+				}
+				break;
 		}
+	},
+	
+	uninitialize : function(){
+	}
+		});
 	});
-});
 
-require([ 'HTMLSnippet/widget/HTMLSnippet' ]);
+require([ "HTMLSnippet/widget/HTMLSnippet" ]);
